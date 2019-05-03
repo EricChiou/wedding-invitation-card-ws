adf46b4abec3fe22dd808e78f074ae8fcd26c789
EricChiou <miatlab@gmail.com>
2019-05-04 02:55:36 +0800

    remove helloworld api

--- main.go
+++ main.go
@@ -25,14 +25,9 @@ func main() {
 	}
 
 	// init api
-	router.GET("/helloWorld", helloWorld)
 	apply.INIT()
 
 	// init http server
 	router.INIT(6200, nil)
 	router.RUNSSL("server.crt", "server.key")
-}
-
-func helloWorld(context *router.Context) {
-	context.Res.Write([]byte("Hello World!!"))
 }
