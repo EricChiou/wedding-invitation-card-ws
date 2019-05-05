c3e04eada8d0267c71164a114ee947b759e254a9
EricChiou <miatlab@gmail.com>
2019-05-06 00:05:32 +0800

    modify cors setting

--- router/router.go
+++ router/router.go
@@ -216,7 +216,7 @@ func checkDuplicate(pathAry []string, targetPathAry []string) bool {
 
 func router() {
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		res.Header().Set("Access-Control-Allow-Origin", "https://www.calicomoo.ml, https://calicomoo.ml")
+		res.Header().Set("Access-Control-Allow-Origin", "*")
 		res.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		switch req.Method {
 		case "GET":
