fc2a0f886ad7483e5eae183dddcd41dbd48307f6
eric.chiou <eric.chiou@acer.com>
2019-05-06 18:03:51 +0800

    modify api header

--- router/router.go
+++ router/router.go
@@ -25,6 +25,8 @@ type Context struct {
 // INIT init api handler
 func INIT() {
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Set("Access-Control-Allow-Origin", "*")
+		res.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		switch req.Method {
 		case "GET":
 			pathHandler(res, req, get)
